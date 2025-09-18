94b2981a6c123b444bedab6309c49ab0b08901b2
amineboukni22-lang <amineboukni22@gmail.com>
2025-09-19 00:11:39 +0200

    Ajout des tests et mise à jour de CMakeLists, ajout d'un .gitignore

---
+++ tests/test-hello.cpp
@@ -0,0 +1,7 @@
+#include <basic/message.h>
+
+int main()
+{
+    hello_world();  // appelle ta fonction
+    return 0;       // retourne 0 = succès
+}
---
+++ tests/test-version.cpp
@@ -0,0 +1,7 @@
+#include <basic/message.h>
+
+int main()
+{
+    print_version();  // appelle une fonction
+    return 1;         // retourne 1 = échec
+}
